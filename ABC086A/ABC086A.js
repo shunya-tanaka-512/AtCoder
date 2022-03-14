5f40c5f09e77661126abded99d1e0014e9c884b0
mami <85916526+mami-tus@users.noreply.github.com>
2022-03-14 23:42:25 +0900

    modify js

--- ABC086A/ABC086A.js
+++ ABC086A/ABC086A.js
@@ -1,12 +1,28 @@
-function main(input) {
-  input = input.split(" ");
-  var a = parseInt(input[0], 10);
-  var b = parseInt(input[1], 10);
-  //積を２で割った余が０ならEven、それ以外をOddで出力
-  if ((a * b) % 2 == 0) {
-    console.log("Even");
-  } else {
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+var input_lines = []; //標準入力から受け取ったデータを格納する配列
+var reader = require("readline").createInterface({
+  //requireという処理でreadlineという機能を用いて標準入力からデータを受け取る
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// ーーーー入力ーーーー
+reader.on("line", (line) => {
+  //line変数には標準入力から渡された一行のデータが格納されている
+  input_lines = line.split(" "); //ここで、input_lines配列に、標準入力から渡されたデータが入る
+});
+
+// ーーーー出力ーーーー
+reader.on("close", () => {
+  //受け取ったデータを用いて処理を行う
+  var a = parseInt(input_lines[0], 10);
+  var b = parseInt(input_lines[1], 10);
+  //aとbどちらも奇数なら積は奇数、それ以外は偶数
+  if (a % 2 != 0 && b % 2 != 0) {
     console.log("Odd");
+  } else {
+    console.log("Even");
   }
-}
-main(require("fs").readFileSync("/dev/stdin", "utf8"));
+});
