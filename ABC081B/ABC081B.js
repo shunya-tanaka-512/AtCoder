04f23619d2aea4aa8f6c5ad5f228ab0cbebdd088
mami <85916526+mami-tus@users.noreply.github.com>
2022-03-15 00:42:38 +0900

    modify js

--- ABC081B/ABC081B.js
+++ ABC081B/ABC081B.js
@@ -1,24 +1,42 @@
-function main(input) {
-  input = input.split('\n'); //[ '3', '8 12 40' ]
-var arr = input[1].split(' '); //[ '8', '12', '40' ]
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+var input_lines = []; //標準入力から受け取ったデータを格納する配列
+var reader = require("readline").createInterface({
+  //requireという処理でreadlineという機能を用いて標準入力からデータを受け取る
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// ーーーー入力ーーーー
+
+reader.on("line", (line) => {
+  //line変数には標準入力から渡された一行のデータが格納されている
+  input_lines.push(line); //[ '3', '8 12 40' ]ここで、input_lines配列に、標準入力から渡されたデータが入る
+});
+
+// ーーーー出力ーーーー
+reader.on("close", () => {
+  //受け取ったデータを用いて処理を行う
+  var arr = input_lines[1].split(" "); //[ '8', '12', '40' ]
   var count = 0;
-  var flag = false;
-while (true) {
-     for ( let j = 0; j < arr.length; j++) {
-       //奇数だったら次の処理へ
-       if (arr[j] % 2 !== 0) {
-         flag = true;
-         break;
+  var isOdd = false;
+  while (true) {
+    for (let i = 0; i < arr.length; i++) {
+      //奇数だったら次の処理へ
+      if (arr[i] % 2 !== 0) {
+        isOdd = true;
+        break;
       }
-     }
-    if (flag){
-    break;
     }
-     //arrを2で割った配列へ代入
-  arr = arr.map(value => value / 2);
+    //奇数だったらwhileを抜ける
+    if (isOdd) {
+      break;
+    }
+    //arrを2で割って配列へ代入
+    arr = arr.map((value) => value / 2);
     count += 1;
   }
 
   console.log(count);
-}
-main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+});
