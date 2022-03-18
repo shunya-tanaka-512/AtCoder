daffe7494947480b83aea0ea0d9ef26536fe3706
mami <85916526+mami-tus@users.noreply.github.com>
2022-03-18 21:04:31 +0900

    modify roop

--- ABC087B/ABC087B.js
+++ ABC087B/ABC087B.js
@@ -1,17 +1,39 @@
-function main(input) {
-  const [a, b, c, x] = input.split('\n');  //[ '2', '2', '2', '100' ]
-let count = 0;
-  //硬貨それぞれの枚数分ループ
-for (let i = 0; i <= a; i++) {
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+var input_lines = []; //標準入力から受け取ったデータを格納する配列
+var reader = require("readline").createInterface({
+  //requireという処理でreadlineという機能を用いて標準入力からデータを受け取る
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// ーーーー入力ーーーー
+
+reader.on("line", (line) => {
+  //line変数には標準入力から渡された一行のデータが格納されている
+  input_lines.push(line); //ここで、input_lines配列に、標準入力から渡されたデータが入る
+});
+
+// ーーーー出力ーーーー
+reader.on("close", () => {
+  //受け取ったデータを用いて処理を行う
+  const [a, b, c, x] = input_lines;
+  let count = 0;
+  for (let i = 0; i <= a; i++) {
+    if (500 * i > x) {
+      break;
+    }
+    tmp = x - 500 * i;
     for (let j = 0; j <= b; j++) {
-        for (let k = 0; k <= c; k++) {
-          if ((500 * i + 100 * j + 50 * k) == x) {
-          count += 1;
-          }
-          }
+      if (100 * j > tmp) {
+        break;
       }
+      if (tmp - 100 * j <= 50 * c) {
+        count += 1;
+      }
+    }
   }
 
   console.log(count);
-}
-main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+});
