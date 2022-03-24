b292b1b8a45e8df4aa4b9c4eacfb4e6bcad2ce0f
mami <85916526+mami-tus@users.noreply.github.com>
2022-03-25 02:27:43 +0900

    2進数を使った解き方で解答

--- ABC081B/ABC081B.py
+++ ABC081B/ABC081B.py
@@ -1,18 +1,8 @@
-intl_val_num = int(input())  # 整数の個数
+int_val_num = int(input())  # 整数の個数
 int_val_list = list(map(int, input().split()))  # 正の整数
-count = 0
-isOdd = False
-while True:
-    for int_val in int_val_list:
-        # 奇数だったら次の処理へ
-        if int_val % 2 != 0:
-            isOdd = True
-            break
-    # 奇数だったらwhileを抜ける
-    if isOdd:
-        break
-    # 2で割ってリストへ代入
-    int_val_list = [int_val / 2 for int_val in int_val_list]
-    count += 1
-
-print(count)
+bin_val_list = [format(int_val, "b") for int_val in int_val_list]  # 2進数変換
+# 文字列反転['0001', '0011', '000101']
+return_bin_val_list = [bin_val[::-1] for bin_val in bin_val_list]
+# 文字列の位置取得[3, 2, 3]
+find_1 = [return_bin_val.find("1") for return_bin_val in return_bin_val_list]
+print(min(find_1))
