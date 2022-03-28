0d72e30918a004ee6ca529854106dff1c335d83e
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-03-29 02:39:18 +0900

    関数名を修正

--- ABC086A/ABC086A.py
+++ ABC086A/ABC086A.py
@@ -1,4 +1,4 @@
-def odd_even(x, y):
+def judge_odd_or_even(x, y):
     if x % 2 != 0 and y % 2 != 0:
         return "Odd"
     else:
--- ABC086A/test_ABC086A.py
+++ ABC086A/test_ABC086A.py
@@ -1,8 +1,8 @@
-from ABC086A import odd_even
+from ABC086A import judge_odd_or_even
 
 
-def test_odd_even():
-    assert odd_even(3, 3) == "Odd"
-    assert odd_even(2, 3) == "Even"
-    assert odd_even(2, 4) == "Even"
-    assert odd_even(1, 10000) == "Even"
+def test_judge_odd_or_even():
+    assert judge_odd_or_even(3, 3) == "Odd"
+    assert judge_odd_or_even(2, 3) == "Even"
+    assert judge_odd_or_even(2, 4) == "Even"
+    assert judge_odd_or_even(1, 10000) == "Even"
