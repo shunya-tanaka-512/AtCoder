e8295f8796cbb10ba16b648090073b43c7934ab8
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-03-30 01:13:51 +0900

    add decorator

--- ABC081B/test_ABC081B.py
+++ ABC081B/test_ABC081B.py
@@ -1,31 +1,14 @@
+import pytest
 from ABC081B import get_times_dividable_in_2
 
 
-def test_get_times_dividable_in_2():
-    assert (
-        get_times_dividable_in_2(
-            [
-                8,
-                12,
-                40,
-            ]
-        )
-        == 2
-    )
-    assert (
-        get_times_dividable_in_2(
-            [
-                5,
-                6,
-                8,
-                10,
-            ]
-        )
-        == 0
-    )
-    assert (
-        get_times_dividable_in_2(
-            [382253568, 723152896, 37802240, 379425024, 404894720, 471526144]
-        )
-        == 8
-    )
+@pytest.mark.parametrize(
+    "param1,expected",
+    [
+        ((8, 12, 40), 2),
+        ((5, 6, 8, 10), 0),
+        ((382253568, 723152896, 37802240, 379425024, 404894720, 471526144), 8),
+    ],
+)
+def test_get_times_dividable_in_2(param1, expected):
+    assert get_times_dividable_in_2(param1) == expected
