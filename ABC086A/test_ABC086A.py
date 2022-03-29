1f8216fa3e86c634c7eee4cf0983d5724358a1ce
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-03-30 00:45:38 +0900

    add decorator

--- ABC086A/test_ABC086A.py
+++ ABC086A/test_ABC086A.py
@@ -1,8 +1,10 @@
+import pytest
 from ABC086A import judge_odd_or_even
 
 
-def test_judge_odd_or_even():
-    assert judge_odd_or_even(3, 3) == "Odd"
-    assert judge_odd_or_even(2, 3) == "Even"
-    assert judge_odd_or_even(2, 4) == "Even"
-    assert judge_odd_or_even(1, 10000) == "Even"
+@pytest.mark.parametrize(
+    "param1,param2,expected",
+    [(3, 3, "Odd"), (2, 3, "Even"), (2, 4, "Even"), (1, 10000, "Even")],
+)
+def test_judge_odd_or_even(param1, param2, expected):
+    assert judge_odd_or_even(param1, param2) == expected
