15291d9fcf70fc4587f22fb4d3e6c81228c5d152
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-03-30 01:39:35 +0900

    add decorator

--- PracticeA/test_PracticeA.py
+++ PracticeA/test_PracticeA.py
@@ -1,6 +1,10 @@
+import pytest
 from PracticeA import process_data
 
 
-def test_process_data():
-    assert process_data(1, 2, 3, "test") == (6, "test")
-    assert process_data(72, 128, 256, "myonmyon") == (456, "myonmyon")
+@pytest.mark.parametrize(
+    "param1,param2,param3,param4,expected",
+    [(1, 2, 3, "test", (6, "test")), (72, 128, 256, "myonmyon", (456, "myonmyon"))],
+)
+def test_process_data(param1, param2, param3, param4, expected):
+    assert process_data(param1, param2, param3, param4) == (expected)
