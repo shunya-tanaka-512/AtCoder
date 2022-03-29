54b71cf685443cf9032a1d5450be719c758a500c
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-03-30 00:34:17 +0900

    add decorator

--- ABC087B/test_ABC087B.py
+++ ABC087B/test_ABC087B.py
@@ -1,7 +1,10 @@
+import pytest
 from ABC087B import get_pattern_select_coins
 
 
-def test_get_pattern_select_coins():
-    assert get_pattern_select_coins(2, 2, 2, 100) == 2
-    assert get_pattern_select_coins(5, 1, 0, 150) == 0
-    assert get_pattern_select_coins(30, 40, 50, 6000) == 213
+@pytest.mark.parametrize(
+    "param1,param2,param3,param4,expected",
+    [(2, 2, 2, 100, 2), (5, 1, 0, 150, 0), (30, 40, 50, 6000, 213)],
+)
+def test_get_pattern_select_coins(param1, param2, param3, param4, expected):
+    assert get_pattern_select_coins(param1, param2, param3, param4) == expected
