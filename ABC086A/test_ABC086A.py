e2b3bdc9fb4e56d5ea68fa3830b05c167b6f8b63
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-03-30 22:25:03 +0900

    modified pytest

--- ABC086A/test_ABC086A.py
+++ ABC086A/test_ABC086A.py
@@ -3,8 +3,13 @@ from ABC086A import judge_odd_or_even
 
 
 @pytest.mark.parametrize(
-    "param1,param2,expected",
-    [(3, 3, "Odd"), (2, 3, "Even"), (2, 4, "Even"), (1, 10000, "Even")],
+    "x, y, expected",
+    [
+        (3, 3, "Odd"),
+        (2, 3, "Even"),
+        (2, 4, "Even"),
+        (1, 10000, "Even")
+    ],
 )
-def test_judge_odd_or_even(param1, param2, expected):
-    assert judge_odd_or_even(param1, param2) == expected
+def test_judge_odd_or_even(x, y, expected):
+    assert judge_odd_or_even(x, y) == expected
