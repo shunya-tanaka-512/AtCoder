cbb7c4a7e0b01e09beb8c9e4c35e42d4f4861cdd
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-03-30 21:48:36 +0900

    modified pytest

--- PracticeA/test_PracticeA.py
+++ PracticeA/test_PracticeA.py
@@ -3,8 +3,11 @@ from PracticeA import process_data
 
 
 @pytest.mark.parametrize(
-    "param1,param2,param3,param4,expected",
-    [(1, 2, 3, "test", (6, "test")), (72, 128, 256, "myonmyon", (456, "myonmyon"))],
+    "a, b, c, s, expected",
+    [
+        (1, 2, 3, "test", (6, "test")),
+        (72, 128, 256, "myonmyon", (456, "myonmyon"))
+    ],
 )
-def test_process_data(param1, param2, param3, param4, expected):
-    assert process_data(param1, param2, param3, param4) == (expected)
+def test_process_data(a, b, c, s, expected):
+    assert process_data(a, b, c, s) == (expected)
