ccd77a50a9ad9a436c6b10368318988d5941b18d
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-03-30 21:57:25 +0900

    modified pytest

--- ABC081A/test_ABC081A.py
+++ ABC081A/test_ABC081A.py
@@ -3,12 +3,12 @@ from ABC081A import get_number_of_marbles
 
 
 @pytest.mark.parametrize(
-    "param,expected",
+    "input_num, expected",
     [
         (101, 2),
         (000, 0),
         (111, 3),
     ],
 )
-def test_get_number_of_marbles(param, expected):
-    assert get_number_of_marbles(param) == expected
+def test_get_number_of_marbles(input_num, expected):
+    assert get_number_of_marbles(input_num) == expected
