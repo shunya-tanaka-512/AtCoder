fb9fe78e794eea33a2ab389e975877f5acf9488b
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-03-30 22:40:12 +0900

    modified pytest

--- ABC087B/test_ABC087B.py
+++ ABC087B/test_ABC087B.py
@@ -3,8 +3,12 @@ from ABC087B import get_pattern_select_coins
 
 
 @pytest.mark.parametrize(
-    "param1,param2,param3,param4,expected",
-    [(2, 2, 2, 100, 2), (5, 1, 0, 150, 0), (30, 40, 50, 6000, 213)],
+    "yen500_num, yen100_num, yen50_num, yen_sum, expected",
+    [
+        (2, 2, 2, 100, 2),
+        (5, 1, 0, 150, 0),
+        (30, 40, 50, 6000, 213)
+    ],
 )
-def test_get_pattern_select_coins(param1, param2, param3, param4, expected):
-    assert get_pattern_select_coins(param1, param2, param3, param4) == expected
+def test_get_pattern_select_coins(yen500_num, yen100_num, yen50_num, yen_sum, expected):
+    assert get_pattern_select_coins(yen500_num, yen100_num, yen50_num, yen_sum) == expected
