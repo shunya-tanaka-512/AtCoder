b3a3aed5c5df3807fc18f718826242836925d50f
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-04-01 04:26:27 +0900

    create test file

---
+++ ABC088B/test_ABC088B.py
@@ -0,0 +1,14 @@
+import pytest
+from ABC088B import get_score_diff
+
+
+@pytest.mark.parametrize(
+    "scores, expected",
+    [
+        ((3, 1), 2),
+        ((2, 7, 4), 5),
+        ((20, 18, 2, 18), 18)
+    ],
+)
+def test_get_score_diff(scores, expected):
+    assert get_score_diff(*scores) == expected
