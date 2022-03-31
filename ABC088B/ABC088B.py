1952a264faed29ab0ae3e9523c13fcc57083bbfc
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-04-01 04:25:49 +0900

    python fileタイポしていたため再度commit

--- ABC088B/ABC088B.py
+++ ABC088B/ABC088B.py
@@ -2,7 +2,7 @@ def get_score_diff(*scores):  # scoresはtuple
     # 奇数回は足す、偶数回は引く
     score_diff = 0
     new_scores = sorted(scores, reverse=True)  # sortedはlistを返す
-    for i, score in enumerate(new_scores):  # enumerateで要素とインデックス番号取得Ï
+    for i, score in enumerate(new_scores):  # enumerateで要素とインデックス番号取得
         if i % 2 == 0:
             score_diff += score
         else:
