fad42eb359accef562ac498ff507acf73bcbd4e8
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-04-03 17:05:37 +0900

    誤答だったので修正

--- ABC086C/ABC086C.py
+++ ABC086C/ABC086C.py
@@ -1,7 +1,6 @@
 def judge_possible_or_impossible(time_point_list: list) -> str:
     for time_point in time_point_list:
         time, point_x, point_y = time_point
-        if time >= (point_x + point_y) and time % 2 == (point_x + point_y) % 2:
-            return "Yes"
-        else:
+        if not (time >= (point_x + point_y) and time % 2 == (point_x + point_y) % 2):
             return "No"
+    return "Yes"
