7b88c340a08e65e715898b608bf205de2d0ba063
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-04-05 01:53:31 +0900

    後ろから探す方法で解答

--- ABC049C/ABC049C.py
+++ ABC049C/ABC049C.py
@@ -1,7 +1,16 @@
 def judge_str_match(character_str: str) -> str:
-    # この順でreplaceしないと文字が被っているところが先にreplaceされてしまう
-    replaced_str = character_str.replace('eraser', '').replace('erase', '').replace('dreamer', '').replace('dream', '')
-    if len(replaced_str) == 0:
+    character_str = character_str[::-1]
+    word_list = ["dream"[::-1], "dreamer"[::-1], "erase"[::-1], "eraser"[::-1]]
+    accu_str = ""  # これをfor文のなかで定義したらだめ
+    judge = False
+    for i in character_str:
+        accu_str += i  # 後ろの文字から一文字ずつ蓄積
+        if accu_str in word_list:  # リストにaccu_strと一致する要素あればTrue
+            accu_str = ""  # 蓄積解除
+            judge = True
+        else:
+            judge = False
+    if judge:
         return "YES"
     else:
         return "NO"
