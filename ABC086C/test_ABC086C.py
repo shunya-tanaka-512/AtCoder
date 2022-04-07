afccc2fde4108631992a07e49fbd32a495b82bbf
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-04-08 01:09:56 +0900

    tupleに変更

--- ABC086C/test_ABC086C.py
+++ ABC086C/test_ABC086C.py
@@ -5,9 +5,9 @@ from ABC086C import judge_possible_or_impossible
 @pytest.mark.parametrize(
     "time_point_list, expected",
     [
-        ([[3, 1, 2], [6, 1, 1]], "Yes"),
-        ([[2, 100, 100]], "No"),
-        ([[5, 1, 1], [100, 1, 1]], "No")
+        ([(3, 1, 2), (6, 1, 1)], "Yes"),
+        ([(2, 100, 100)], "No"),
+        ([(5, 1, 1), (100, 1, 1)], "No")
     ],
 )
 def test_judge_possible_or_impossible(time_point_list: list, expected: str):
