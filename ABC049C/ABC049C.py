23647d721ea4423cd71a630e392792481f153d2d
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-04-07 23:03:40 +0900

    while文で解答

--- ABC049C/ABC049C.py
+++ ABC049C/ABC049C.py
@@ -1,16 +1,16 @@
 def judge_str_match(character_str: str) -> str:
     character_str = character_str[::-1]
     word_list = ["dream"[::-1], "dreamer"[::-1], "erase"[::-1], "eraser"[::-1]]
-    accu_str = ""  # これをfor文のなかで定義したらだめ
-    judge = False
-    for i in character_str:
-        accu_str += i  # 後ろの文字から一文字ずつ蓄積
-        if accu_str in word_list:  # リストにaccu_strと一致する要素あればTrue
-            accu_str = ""  # 蓄積解除
-            judge = True
+    while len(character_str) != 0:
+        if character_str[:5] == word_list[0]:
+            character_str = character_str[5:]
+        elif character_str[:7] == word_list[1]:
+            character_str = character_str[7:]
+        elif character_str[:5] == word_list[2]:
+            character_str = character_str[5:]
+        elif character_str[:6] == word_list[3]:
+            character_str = character_str[6:]
         else:
-            judge = False
-    if judge:
-        return "YES"
-    else:
-        return "NO"
+            return "NO"
+            break
+    return "YES"
