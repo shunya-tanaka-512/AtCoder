67fed27abecd4e1e95da958da0cc55246af37e5c
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-04-24 11:50:33 +0900

    変数名を変更

--- typical90j/typical90j.py
+++ typical90j/typical90j.py
@@ -5,18 +5,18 @@ def get_score_totals(n: int, classes_and_scores: list, q: int, id_nums: list) ->
     scores = [d[1] for d in classes_and_scores]
     score_totals = []
 
-    score_accms = [[0, 0]]  # 0のインデックス追加
-    score_accm_1 = 0
-    score_accm_2 = 0
+    accumulations = [[0, 0]]  # 0のインデックス追加
+    accumulation_1 = 0
+    accumulation_2 = 0
     for i in range(n):
         if classes[i] == 1:
-            score_accm_1 += scores[i]
+            accumulation_1 += scores[i]
         else:
-            score_accm_2 += scores[i]
-        score_accms.append([score_accm_1, score_accm_2])
+            accumulation_2 += scores[i]
+        accumulations.append([accumulation_1, accumulation_2])
 
     for j in range(q):
-        score_total_1 = score_accms[r_id_nums[j]][0] - score_accms[l_id_nums[j]][0]
-        score_total_2 = score_accms[r_id_nums[j]][1] - score_accms[l_id_nums[j]][1]
+        score_total_1 = accumulations[r_id_nums[j]][0] - accumulations[l_id_nums[j]][0]
+        score_total_2 = accumulations[r_id_nums[j]][1] - accumulations[l_id_nums[j]][1]
         score_totals.append([score_total_1, score_total_2])
     return score_totals
