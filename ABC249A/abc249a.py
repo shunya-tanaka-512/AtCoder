f2c2c11c1d03563a9b5f1f277180d32bfdc1faef
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-04-30 02:35:32 +0900

    クラスを定義した

--- ABC249A/abc249a.py
+++ ABC249A/abc249a.py
@@ -1,17 +1,23 @@
-a, b, c, d, e, f, x = map(int, input().split())
-# a + cで割った余りがaより小さい時
-if a > (x % (a + c)):
-    takahashi_time = (x // (a + c)) * a + x % (a + c)
-# a + cで割った余りがa以上->あと１回a秒歩く+休み(ノーカウント)
-else:
-    takahashi_time = (x // (a + c)) * a + a
-takahashi_dist = takahashi_time * b
+class Person:
+    def __init__(self, walk_time, per_second, break_time):
+        self.walk_time = walk_time
+        self.per_second = per_second
+        self.break_time = break_time
 
-if d > (x % (d + f)):
-    aoki_time = (x // (d + f)) * d + x % (d + f)
-else:
-    aoki_time = (x // (d + f)) * d + d
-aoki_dist = aoki_time * e
+    def calc_dist_walked(self, time):
+        if self.walk_time > (time % (self.walk_time + self.break_time)):
+            time_walked = (time // (self.walk_time + self.break_time)) * self.walk_time + time % (self.walk_time + self.break_time)
+        else:
+            time_walked = (time // (self.walk_time + self.break_time)) * self.walk_time + self.walk_time
+        dist_walked = time_walked * self.per_second
+        return dist_walked
+
+
+a, b, c, d, e, f, x = map(int, input().split())
+takahashi = Person(a, b, c)  # インスタンス化
+aoki = Person(d, e, f)
+takahashi_dist = takahashi.calc_dist_walked(x)
+aoki_dist = aoki.calc_dist_walked(x)
 
 if takahashi_dist > aoki_dist:
     print("Takahashi")
