e9941564aecd61599cce704f943f54ecaa2dfde3
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-05-05 10:58:38 +0900

    if文使わず書き換え

--- ABC248D/abc248d.py
+++ ABC248D/abc248d.py
@@ -3,7 +3,4 @@ a = list(map(int, input().split()))
 q = int(input())
 queries = [(map(int, input().split())) for _ in range(q)]
 for (l, r, x) in queries:
-    if x in a[(l - 1):r]:
-        print(a[(l - 1):r].count(x))
-    else:
-        print(0)
+    print(a[(l - 1):r].count(x))
