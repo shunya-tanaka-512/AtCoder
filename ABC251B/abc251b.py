b73e6c7a068ecbc53b401894f252cc4f17a8e643
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-05-27 01:48:55 +0900

    0の選択肢を追加

--- ABC251B/abc251b.py
+++ ABC251B/abc251b.py
@@ -1,15 +1,15 @@
 import itertools
 
 n, w = map(int, input().split())
-weights = list(map(int, input().split()))
+# 重さが0のおもりが2個あるとする
+weights = list(map(int, input().split())) + [0, 0]
 nums = []  # 良い整数のリスト
-for i in range(1, 4):
-    num_combinations = list(itertools.combinations(weights, i))
-    # num_combinationsの中で組み合わせ同士の和を調べる
-    num_combinations_sums = list(map(sum, num_combinations))
-    for j in num_combinations_sums:
-        if j <= w:
-            nums.append(j)
+num_combinations = list(itertools.combinations(weights, 3))
+# num_combinationsの中で組み合わせ同士の和を調べる
+num_combinations_sums = list(map(sum, num_combinations))
+for j in num_combinations_sums:
+    if j <= w:
+        nums.append(j)
 # 被っているリストを排除
 nums = set(nums)
 print(len(nums))
