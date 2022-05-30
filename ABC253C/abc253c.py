42e2eda3fc6a31333c9b934026211770ad5d2c41
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-05-31 00:01:47 +0900

    elif使う

--- ABC253C/abc253c.py
+++ ABC253C/abc253c.py
@@ -4,9 +4,9 @@ s = []
 for i in range(q):
     if queries[i][0] == 1:
         s.append(queries[i][1])
-    if queries[i][0] == 2:
+    elif queries[i][0] == 2:
         remove_count = min(queries[i][2], s.count(queries[i][1]))
         for j in range(remove_count):
             s.remove(queries[i][1])
-    if queries[i][0] == 3:
+    else:
         print(max(s) - min(s))
