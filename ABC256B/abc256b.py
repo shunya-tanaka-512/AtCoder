55408a5847d731a8ec116b83c7af66ae906f3d37
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-06-26 14:03:41 +0900

    Baseballクラス作成

--- ABC256B/abc256b.py
+++ ABC256B/abc256b.py
@@ -1,13 +1,19 @@
+class Baseball:
+    def __init__(self, num_of_batters, num_of_bases_advanced):
+        self.num_of_batters = num_of_batters
+        self.num_of_bases_advanced = num_of_bases_advanced
+
+    def get_scores(self):
+        num_of_bases_with_batter = [0] * self.num_of_batters
+        for i in range(self.num_of_batters):
+            for j in range(i+1):
+                num_of_bases_with_batter[j] = num_of_bases_with_batter[j] + self.num_of_bases_advanced[i]
+        scores = len([k for k in num_of_bases_with_batter if k >= 4])
+        return scores
+
+
 N = int(input())
 A = list(map(int, input().split()))
-P = 0
-# コマがどのマスにいるか管理するリスト用意
-# Aのリストから一つずつ順に値を取り出す
-# 取り出した値を、その値のインデックス以下のコマ全部にたす
-# 上の処理が終わった時点でコマの値が４以上である個数が求めたいPの値になる
-batters = [0] * N
-for i in range(N):
-    for j in range(i+1):
-        batters[j] = batters[j] + A[i]
-P = len([k for k in batters if k >= 4])
-print(P)
+baseball_game = Baseball(N, A)
+game_scores = baseball_game.get_scores()
+print(game_scores)
