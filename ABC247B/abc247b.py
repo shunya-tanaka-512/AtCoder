42368f42255e1c91bdd58486a9c18e8d4c5027e3
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-06-26 12:26:31 +0900

    breakを追加

--- ABC247B/abc247b.py
+++ ABC247B/abc247b.py
@@ -1,20 +1,20 @@
 N = int(input())
 names = [list(input().split()) for _ in range(N)]
-# まず姓で調べて他の人と一致したかどうかを記録しておく
-# 次に名で調べて、姓名どちらも他の人と一致していたらNoを出力して終了
-# どちらか一致しなかったら次へ
-# 最後までNoにならずに処理が終了したらYes
+
 for i in range(N):
     is_s_matched = False
     for nickname in names[i]:
         for j in range(N):
             if i != j:
-                # is_s_matchedがTrue時に名前も一致していたらNo出力して終了
+                # names[i]の姓も名も自分以外の人の姓および名と一致していたらNo出力して終了
                 if is_s_matched:
                     if nickname in names[j]:
                         print("No")
                         exit()
-                # 他の人と名字が一致している
+                # names[i]の姓が自分以外の人の姓および名と一致しているときis_s_matchedがTrueにする
                 elif nickname in names[j]:
                     is_s_matched = True
+                    break  # 姓はもう調べなくていいので名を調べる
+        if not is_s_matched:
+            break  # names[i]の姓が自分以外の人の姓および名のどちらとも一致しないので次のnamesへ
 print("Yes")
