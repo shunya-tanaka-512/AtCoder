74434dd15b74861615af9073b6f382119762a42b
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-07-04 06:50:39 +0900

    計算で一般化

--- ABC257A/abc257a.py
+++ ABC257A/abc257a.py
@@ -1,9 +1,6 @@
+import math
+
+
 N, X = map(int, input().split())
-# chr関数でN回分、65~90を回す
-# 回数をカウントしてX番目で出力
-count = 0
-for i in range(65, 91):
-    for j in range(N):
-        count += 1
-        if X == count:
-            print(chr(i))
+num = math.ceil(X / N) - 1
+print(chr(ord("A") + num))
